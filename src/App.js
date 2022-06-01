e800c820434c18ba42d4d521725e3e2329138567
Timothy Lai <timothy.lai@smartbear.com>
2022-06-01 12:49:20 -0700

    feat: babel config and dependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-// import SwaggerUI from 'swagger-ui';
+// import SwaggerUI from 'swagger-ui-react';
+import SwaggerUI from 'swagger-ui';
+// import 'swagger-ui/dist/swagger-ui.css';
+
+// var SwaggerUIBundle = require('swagger-ui-dist').SwaggerUIBundle
+
+// const ui = SwaggerUIBundle({
+//   url: "https://petstore.swagger.io/v2/swagger.json",
+//   dom_id: '#swagger-ui',
+//   presets: [
+//     SwaggerUIBundle.presets.apis,
+//     SwaggerUIBundle.SwaggerUIStandalonePreset
+//   ],
+//   layout: "StandaloneLayout"
+// })
 
 export function App() {
   return <h1>Hello world from React!</h1>;
+  // return (
+  //   <div className="swagger">
+  //     <h1>still rendering</h1>
+  //     <SwaggerUI />
+  //   </div>
+  // )
 }
