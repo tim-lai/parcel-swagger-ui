7599404235925c680a4e45588966a077be91eadf
Timothy Lai <timothy.lai@smartbear.com>
2022-06-02 14:24:19 -0700

    feat: swagger-ui-bundle as react class with css

---
+++ src/AppAsClass.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import SwaggerUI from 'swagger-ui/dist/swagger-ui-es-bundle';
+
+class App extends React.Component{
+  componentDidMount() {
+    window.ui = SwaggerUI({
+      url: 'https://petstore.swagger.io/v2/swagger.json',
+      dom_id: '#swagger-ui-root'
+    });
+  }
+  render() {
+    return (
+      <div>
+        <div id="swagger-ui-root"></div>
+      </div>
+    )
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import ReactDOM from "react-dom";
-import { App } from "./App";
+import ReactDOM from 'react-dom';
+import App from './AppAsClass';
+import 'swagger-ui/dist/swagger-ui.css';
 
-const app = document.getElementById("swagger-ui");
+const app = document.getElementById('swagger-ui');
 ReactDOM.render(<App />, app);
